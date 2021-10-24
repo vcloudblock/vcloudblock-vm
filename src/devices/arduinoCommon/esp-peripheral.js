ef6c2c70c2c22383f8a691522a350e3406299151
ArthurZheng <869705086@qq.com>
2021-10-24 21:40:33 +0800

    fix bug: can not recive serial port message while using microbit

--- src/devices/arduinoCommon/esp-peripheral.js
+++ src/devices/arduinoCommon/esp-peripheral.js
@@ -176,7 +176,9 @@ class EspPeripheral{
      * @param {object} base64 - the incoming serialport data.
      * @private
      */
-    _onMessage () {
+    _onMessage (base64) {
+        const consoleData = Buffer.from(base64, 'base64');
+        this._runtime.emit(this._runtime.constructor.PERIPHERAL_RECIVE_DATA, consoleData);
     }
 }
 
