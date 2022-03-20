3f1d2b7082fb77b3a0100814f587efc5be19c193
ArthurZheng <869705086@qq.com>
2022-03-20 21:33:03 +0800

    change router name to openblock

--- src/util/scratch-link-websocket.js
+++ src/util/scratch-link-websocket.js
@@ -25,16 +25,16 @@ class ScratchLinkWebSocket {
     open () {
         switch (this._type) {
         case 'BLE':
-            this._ws = new WebSocket('wss://127.0.0.1:20111/scratch/ble');
+            this._ws = new WebSocket('wss://127.0.0.1:20111/openblock/ble');
             break;
         case 'BT':
-            this._ws = new WebSocket('wss://device-manager.scratch.mit.edu:20110/scratch/bt');
+            this._ws = new WebSocket('wss://127.0.0.1:20111/openblock/bt');
             break;
         case 'SERIALPORT':
-            this._ws = new WebSocket('wss://127.0.0.1:20111/scratch/serialport');
+            this._ws = new WebSocket('wss://127.0.0.1:20111/openblock/serialport');
             break;
         default:
-            throw new Error(`Unknown ScratchLink socket Type: ${this._type}`);
+            throw new Error(`Unknown OpenblockLink socket Type: ${this._type}`);
         }
 
         if (this._onOpen && this._onClose && this._onError && this._handleMessage) {
