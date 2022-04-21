303ae356cd81ab4a7b3916da5164b3074074f0cd
ArthurZheng <869705086@qq.com>
2022-04-21 12:49:19 +0800

    add arduinoNano in deivce manager

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -30,6 +30,7 @@ const builtinExtensions = {
 
 const builtinDevices = {
     arduinoUno: () => require('../devices/arduinoUno'),
+    arduinoNano: () => require('../devices/arduinoNano'),
     arduinoUnoUltra: () => require('../devices/arduinoUnoUltra'), // Add A6, A7 pins some customized board
     arduinoLeonardo: () => require('../devices/arduinoLeonardo'),
     arduinoMega2560: () => require('../devices/arduinoMega2560'),
