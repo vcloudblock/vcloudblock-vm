60be5b4cb92a10132748ee82a6a91b02cc00ff3f
ArthurZheng <869705086@qq.com>
2023-07-22 20:03:34 +0800

    Plugin that adds edtion parameter to access resource server to filter non-community edition content

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -275,7 +275,7 @@ class ExtensionManager {
      */
     getDeviceList () {
         return new Promise(resolve => {
-            fetch(`${localResourcesServerUrl}devices/${formatMessage.setup().locale}.json`)
+            fetch(`${localResourcesServerUrl}devices/cmtye/${formatMessage.setup().locale}.json`)
                 .then(response => response.json())
                 .then(devices => {
                     devices = devices.map(dev => {
@@ -362,7 +362,7 @@ class ExtensionManager {
      */
     getDeviceExtensionsList () {
         return new Promise(resolve => {
-            fetch(`${localResourcesServerUrl}extensions/${formatMessage.setup().locale}.json`)
+            fetch(`${localResourcesServerUrl}extensions/cmtye/${formatMessage.setup().locale}.json`)
                 .then(response => response.json())
                 .then(extensions => {
                     extensions = extensions.map(extension => {
