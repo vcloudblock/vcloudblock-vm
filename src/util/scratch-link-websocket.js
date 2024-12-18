865d5a7e03b4a1b7b3f8e4b515f247065a6dce85
netboy <chenshouw@gmail.com>
2024-12-18 17:27:22 +0800

    update local setting

--- src/util/scratch-link-websocket.js
+++ src/util/scratch-link-websocket.js
@@ -25,16 +25,16 @@ class ScratchLinkWebSocket {
     open () {
         switch (this._type) {
         case 'BLE':
-            this._ws = new WebSocket('ws://127.0.0.1:20111/openblock/ble');
+            this._ws = new WebSocket('ws://127.0.0.1:20111/vcloudblock/ble');
             break;
         case 'BT':
-            this._ws = new WebSocket('ws://127.0.0.1:20111/openblock/bt');
+            this._ws = new WebSocket('ws://127.0.0.1:20111/vcloudblock/bt');
             break;
         case 'SERIALPORT':
-            this._ws = new WebSocket('ws://127.0.0.1:20111/openblock/serialport');
+            this._ws = new WebSocket('ws://127.0.0.1:20111/vcloudblock/serialport');
             break;
         default:
-            throw new Error(`Unknown OpenblockLink socket Type: ${this._type}`);
+            throw new Error(`Unknown VCloudblockLink socket Type: ${this._type}`);
         }
 
         if (this._onOpen && this._onClose && this._onError && this._handleMessage) {
